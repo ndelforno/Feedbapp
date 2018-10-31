5bda95175bec25fba658cc10c50b85d71e34224d
Ndelforno <nicolas.delforno@gmail.com>
2018-10-31 18:00:36 -0400

    added view for response creation

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,2 +1,18 @@
 class ResponsesController < ApplicationController
+
+  def new
+    @response = Response.new
+    @request = Request.find(params[:request_id])
+  end
+
+  def create
+    @response = Response.new
+    @request = Request.find(params[:request_id])
+
+    @response.user = current_user
+    @response.request_id = @request.id
+    @response.choice_id = params[:request][:choice_id]
+    @response.comment = params[:response][:comment]
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root 'requests#index'
   resources :requests do
     resources :choices
+    resources :responses
   end
+
   resources :users, only: [:new, :create, :show]
-  resources :responses
   resources :sessions, only: [:new, :create, :destroy]
 end
