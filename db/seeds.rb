2d2f8345159bf4197c0bba0575d4248ca682fe03
Ndelforno <nicolas.delforno@gmail.com>
2018-11-24 13:06:57 -0500

    removed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.destroy_all
-Request.destroy_all
-Response.destroy_all
-Choice.destroy_all
+# User.destroy_all
+# Request.destroy_all
+# Response.destroy_all
+# Choice.destroy_all
 
 # 5.times do
 # User.create(
