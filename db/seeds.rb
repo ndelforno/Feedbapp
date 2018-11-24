28c4009ebc85f8a6a64b4e97670ddf9d7108f30e
Ndelforno <nicolas.delforno@gmail.com>
2018-11-24 12:05:49 -0500

    changed user_id to force int

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,40 +11,40 @@ Request.destroy_all
 Response.destroy_all
 Choice.destroy_all
 
-5.times do
-User.create(
-  first_name: Faker::Name.first_name,
-  email: Faker::Internet.free_email,
-  password: "123456",
-  status: "student",
-  last_name: Faker::Name.last_name
-)
-end
-
-
-10.times do
-Request.create(
-  question: "do you like this design ?",
-  user: User.all.sample,
-  image: 'app/assets/images/feedbapp_test_image.png'
-)
-end
-
-Request.all.each do |request|
-choice1 = Choice.create(
-  choice_text: "Yes",
-  request_id: request.id
-)
-choice2 = Choice.create(
-  choice_text: "No",
-  request_id: request.id
-)
-end
-
-10.times do
-Response.create(
-  user_id: User.all.sample,
-  request_id: Request.all.sample,
-  choice_id: Request.choices.all.sample,
-)
-end
+# 5.times do
+# User.create(
+#   first_name: Faker::Name.first_name,
+#   email: Faker::Internet.free_email,
+#   password: "123456",
+#   status: "student",
+#   last_name: Faker::Name.last_name
+# )
+# end
+#
+#
+# 10.times do
+# Request.create(
+#   question: "do you like this design ?",
+#   user: User.all.sample,
+#   image: 'app/assets/images/feedbapp_test_image.png'
+# )
+# end
+#
+# Request.all.each do |request|
+# choice1 = Choice.create(
+#   choice_text: "Yes",
+#   request_id: request.id
+# )
+# choice2 = Choice.create(
+#   choice_text: "No",
+#   request_id: request.id
+# )
+# end
+#
+# 10.times do
+# Response.create(
+#   user_id: User.all.sample,
+#   request_id: Request.all.sample,
+#   choice_id: Request.choices.all.sample,
+# )
+# end
