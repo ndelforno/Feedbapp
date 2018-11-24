df01e3c6aff2ed0b396baea8fde11d96871b48ec
Ndelforno <nicolas.delforno@gmail.com>
2018-11-24 12:07:15 -0500

    changed user_id to force int

--- db/migrate/20181020152343_data_type_change.rb
+++ db/migrate/20181020152343_data_type_change.rb
@@ -5,8 +5,8 @@ class DataTypeChange < ActiveRecord::Migration[5.2]
     remove_column :responses, :integer
     remove_column :requests, :integer
     remove_column :users, :string
-    change_column :requests, :user_id, :integer, USING user_id::integer
-    change_column :responses, :user_id, :integer, USING user_id::integer
-    change_column :responses, :choice_id, :integer, USING choice_id::integer
+    change_column :requests, :user_id, :integer, "USING user_id::integer"
+    change_column :responses, :user_id, :integer, "USING user_id::integer"
+    change_column :responses, :choice_id, :integer, "USING choice_id::integer"
   end
 end
